d533c687d7ede43dea3386ffbfb7041e0464b49c
capellinx <capellaaa7@gmail.com>
2024-10-20 02:32:22 -0300

    feat: instance helmet and handle-errors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
-import express, { json }  from "express";
-
-export const app = express();
+import "express-async-errors"
+import express, { json } from "express";
+import helmet from "helmet"
+import { errorHandler } from "./middlewares/handle-errors.middleware";
+import { NextFunction, Request, Response } from "express-serve-static-core";
+const app = express();
 
 app.use(json())
+app.use(helmet())
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => { errorHandler.execute });
+
+export { app }
