ad619e62c9680eb7072b164a55c2e0bd49a26fa3
capellinx <capellaaa7@gmail.com>
2024-10-20 02:58:38 -0300

    fitx: update type in params

--- src/repositories/collaborator.ts
+++ src/repositories/collaborator.ts
@@ -1,6 +1,7 @@
+import { Collaborator } from "../domain/entities/collaborator";
 import { CreateCollaboratorDTO } from "../use-case/collaborator/create-collaborator/create-collaborator-dto";
 
 export interface CollaboratorRepository {
-   create(payload: CreateCollaboratorDTO): Promise<void>
+   create(payload: Collaborator): Promise<void>
    findByEmail(email: string): Promise<CreateCollaboratorDTO | null>
 }
