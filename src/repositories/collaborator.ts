b351636ebbb5d35c5a65e331f3113825530efadc
capellinx <capellaaa7@gmail.com>
2024-10-21 22:22:57 -0300

    refactor: change archives of local

--- src/repositories/collaborator.ts
+++
@@ -1,7 +0,0 @@
-import { Collaborator } from "../domain/entities/collaborator";
-import { CreateCollaboratorDTO } from "../use-case/collaborator/create-collaborator/create-collaborator-dto";
-
-export interface CollaboratorRepository {
-   create(payload: Collaborator): Promise<void>
-   findByEmail(email: string): Promise<CreateCollaboratorDTO | null>
-}
--- src/services/encrypted-password.service.ts
+++
@@ -1,5 +0,0 @@
-
-export interface EncryptedPasswordService {
-   encrypt(password: string): Promise<string>
-   comapare(password: string, encryptedPassword: string): Promise<boolean>
-}
