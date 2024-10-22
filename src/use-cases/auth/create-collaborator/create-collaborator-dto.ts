857e7a9bb3ca8d177de16e1e4e797c9d4f8aa63a
capellinx <capellaaa7@gmail.com>
2024-10-21 22:26:47 -0300

    feat: create DTO to register collaborator

---
+++ src/use-cases/auth/create-collaborator/create-collaborator-dto.ts
@@ -0,0 +1,14 @@
+import { PersonType, Role } from "@prisma/client";
+import { z } from "zod";
+
+export const collaboratorSchema = z.object({
+   name: z.string().min(1),
+   email: z.string().email().min(1),
+   matricula: z.string(),
+   cpf: z.coerce.string().min(1).max(11),
+   phone: z.coerce.string().min(1).max(14),
+   personType: z.nativeEnum(PersonType),
+   role: z.nativeEnum(Role).optional(),
+})
+
+export type CreateCollaboratorDTO = z.infer<typeof collaboratorSchema>
