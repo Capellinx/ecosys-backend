2f1d3603fae9d4410b946a8e7b3e7385090be214
capellinx <capellaaa7@gmail.com>
2024-10-21 23:58:10 -0300

    feat: create controller to approve collaborator

---
+++ src/use-cases/collaborator/approve-collaborator/approve-collaborator.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { ApproveCollaboratorUseCase } from "./approve-collaborator";
+
+
+export class ApproveCollaboratorController {
+   constructor(
+      private approveCollaboratorUseCase: ApproveCollaboratorUseCase
+   ) { }
+
+   async handle(request: Request, response: Response): Promise<Response> {
+      const { id } = request.params
+
+      const { success } = await this.approveCollaboratorUseCase.execute(id)
+
+      return response.status(200).send(success)
+   }
+}
