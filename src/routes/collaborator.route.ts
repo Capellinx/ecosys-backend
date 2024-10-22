c780799c9b7b3f2969e68f0ecce58e2ac196e147
capellinx <capellaaa7@gmail.com>
2024-10-21 23:57:51 -0300

    feat: create new route collaborator to approve in app

--- src/routes/collaborator.route.ts
+++ src/routes/collaborator.route.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { createCollaboratorController } from "../use-cases/auth/create-collaborator";
 import { ZodRequestValidate } from "../middleware/zod-errors.middleware";
 import { createCollaboratorSchema } from "../use-cases/auth/create-collaborator/create-collaborator-dto";
+import { approveCollaboratorController } from "../use-cases/collaborator/approve-collaborator";
+import { approveCollaboratorSCchema } from "../use-cases/collaborator/approve-collaborator/approve-collaborator-dto";
 
 
 export const collaboratorRouter = Router()
@@ -11,5 +13,13 @@ collaboratorRouter.post(
    ZodRequestValidate.execute({ body: createCollaboratorSchema }),
    async (request, response) => {
       return await createCollaboratorController.handle(request, response)
+   }
+)
+
+collaboratorRouter.post(
+   "/collaborator/:id/approve",
+   ZodRequestValidate.execute({ params: approveCollaboratorSCchema}),
+   async (request, response) => {
+      return await approveCollaboratorController.handle(request, response)
    }
 )
