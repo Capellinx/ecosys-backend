73a2da43d4bf6e694219afe2b7891b44c9f4da4b
capellinx <capellaaa7@gmail.com>
2024-10-21 23:27:00 -0300

    feat: implements zod validate body

--- src/routes/collaborator.route.ts
+++ src/routes/collaborator.route.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { createCollaboratorController } from "../use-cases/auth/create-collaborator";
+import { ZodRequestValidate } from "../middleware/zod-errors.middleware";
+import { createCollaboratorSchema } from "../use-cases/auth/create-collaborator/create-collaborator-dto";
 
 
 export const collaboratorRouter = Router()
 
 collaboratorRouter.post(
    "/collaborator",
+   ZodRequestValidate.execute({ body: createCollaboratorSchema }),
    async (request, response) => {
       return await createCollaboratorController.handle(request, response)
    }
