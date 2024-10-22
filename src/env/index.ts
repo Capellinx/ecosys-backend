88cbc0736d4731a6e0a5dc13af807c511866575c
capellinx <capellaaa7@gmail.com>
2024-10-21 23:38:56 -0300

    feat: create a new typing to refresh jwt secret

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
    DATABASE_URL: z.string().describe("Database URL"),
    PORT: z.coerce.number().default(3000).describe("Server port"),
    JWT_SECRET: z.string().describe("JWT secret"),
+   JWT_REFRESH_SECRET: z.string().describe("JWT secret"),
 })
 
 const _env = envSchema.safeParse(process.env)
