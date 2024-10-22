211f23632b231fa403cfefe7fb11debf93fe4e7a
capellinx <capellaaa7@gmail.com>
2024-10-21 23:17:09 -0300

    feat: instance the collaboratorRouter

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import "express-async-errors"
 import express, { json } from "express";
 import helmet from "helmet"
 import { errorHandler } from "./middleware/handle-errors.middleware";
-import { NextFunction, Request, Response } from "express-serve-static-core";
+import { collaboratorRouter } from "./routes/collaborator.route";
 const app = express();
 
 app.use(json())
 app.use(helmet())
+
+app.use(collaboratorRouter)
 
 app.use(errorHandler.execute);
 
