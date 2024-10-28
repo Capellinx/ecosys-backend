dc2a987750f9c40dbda577f193a8dd6d18464ad0
capellinx <capellaaa7@gmail.com>
2024-10-28 14:44:06 -0300

    feat: update return and change method to async

--- src/domain/services/encrypted-password.service.ts
+++ src/domain/services/encrypted-password.service.ts
@@ -1,5 +1,5 @@
 
 export interface EncryptedPasswordService {
    encrypt(password: string): Promise<string>
-   comapare(password: string, encryptedPassword: string): Promise<boolean>
+   compare(password: string, encryptedPassword: string): Promise<boolean>
 }
--- src/infra/services/bcrypt.service.ts
+++ src/infra/services/bcrypt.service.ts
@@ -4,11 +4,11 @@ import bcrypt from "bcrypt"
 export class BcryptService implements EncryptedPasswordService{
    public static readonly saltsRounds = 10
 
-   encrypt(password: string): Promise<string> {
-      return bcrypt.hash(password, BcryptService.saltsRounds)
+   async encrypt(password: string): Promise<string> {
+      return await bcrypt.hash(password, BcryptService.saltsRounds)
    }
 
-   comapare(password: string, encryptedPassword: string): Promise<boolean> {
-       return bcrypt.compare(password, encryptedPassword)
+   async compare(password: string, encryptedPassword: string): Promise<boolean> {
+      return await bcrypt.compare(password, encryptedPassword)
    }
 }
