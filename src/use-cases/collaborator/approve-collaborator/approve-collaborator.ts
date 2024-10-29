299373a88a1dbb9854abfa82888a549fdd3bdb96
capellinx <capellaaa7@gmail.com>
2024-10-29 01:15:29 -0300

    feat: implements email service and bcrypt

--- src/use-cases/collaborator/approve-collaborator/approve-collaborator.ts
+++ src/use-cases/collaborator/approve-collaborator/approve-collaborator.ts
@@ -1,19 +1,43 @@
-import { NotFoundError } from "../../../config/application-errors";
+import { BadRequestError, NotFoundError } from "../../../config/application-errors";
 import { CollaboratorRepository } from "../../../domain/repositories/collaborator-repo";
+import { EmailService } from "../../../domain/services/email.service";
+import { EncryptedPasswordService } from "../../../domain/services/encrypted-password.service";
+import { GenerateRandomPass, template } from "../../../utils";
 
 export class ApproveCollaboratorUseCase {
    constructor(
-      private collaboratorRepository: CollaboratorRepository
+      private collaboratorRepository: CollaboratorRepository,
+      private emailService: EmailService,
+      private encryptedPasswordService: EncryptedPasswordService
    ) {}
 
    async execute(id: string): Promise<ApproveCollaboratorUseCase.Output> {
       const collaborator = await this.collaboratorRepository.findById(id)
 
-      if(!collaborator) throw new NotFoundError("Collaborator not found")
+      if(!collaborator) throw new BadRequestError("Collaborator not found")
+      
+      if(collaborator.registration_status === "APPROVED") throw new NotFoundError("Collaborator already approved")
 
-      await this.collaboratorRepository.approve(id)
+      const newPassword = GenerateRandomPass.generate()
+      console.log(newPassword);
+      const encryptPassword = await this.encryptedPasswordService.encrypt(newPassword)
 
-      // implements email service to notify user that his account was approved
+      await Promise.all([
+         await this.collaboratorRepository.approve(id),
+         await this.collaboratorRepository.updatePassword(id, encryptPassword),
+         await this.emailService.send({
+            to: {
+               name: collaborator.name,
+               email: collaborator.email
+            },
+            from: {
+               name: 'EcoSys',
+               email: 'suporte@ecosys.com.br'
+            },
+            subject: 'Ecosys - Acesso Liberado',
+            body: template.approve(newPassword)
+         })
+      ])
 
       return {
          success: {
