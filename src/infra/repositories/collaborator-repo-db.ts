ecab94c63befe1ea46387b935261e75256fb090a
capellinx <capellaaa7@gmail.com>
2024-10-29 01:14:57 -0300

    feat: create method to change password

--- src/infra/repositories/collaborator-repo-db.ts
+++ src/infra/repositories/collaborator-repo-db.ts
@@ -4,25 +4,25 @@ import { CollaboratorRepository } from "../../domain/repositories/collaborator-r
 import { CreateCollaboratorDTO } from "../../use-cases/auth/create-collaborator/create-collaborator-dto";
 
 
-export class CollaboratorRepoDB implements CollaboratorRepository{
-  async create({cpf, email, matricula, name, phone, personType, role, password ,createdAt, updatedAt, unity_conservation}: CollaboratorRepository.CreateParams): Promise<void> {
-      await prisma.collaborator.create({
-         data: {
-            name,
-            email,
-            matricula,
-            cpf,
-            phone,
-            password,
-            unity_conservation,
-            person_type: personType,
-            role,
-            createdAt,
-            updatedAt
-         }
-      })
+export class CollaboratorRepoDB implements CollaboratorRepository {
+  async create({ cpf, email, matricula, name, phone, personType, role, password, createdAt, updatedAt, unity_conservation }: CollaboratorRepository.CreateParams): Promise<void> {
+    await prisma.collaborator.create({
+      data: {
+        name,
+        email,
+        matricula,
+        cpf,
+        phone,
+        password,
+        unity_conservation,
+        person_type: personType,
+        role,
+        createdAt,
+        updatedAt
+      }
+    })
 
-      return
+    return
   }
 
   async findByEmail(email: string): Promise<Collaborator | null> {
@@ -37,17 +37,14 @@ export class CollaboratorRepoDB implements CollaboratorRepository{
     return null
   }
 
-  async findById(id: string): Promise<string | null> {
-     const collaborator = await prisma.collaborator.findUnique({
+  async findById(id: string): Promise<Collaborator | null> {
+    const collaborator = await prisma.collaborator.findUnique({
       where: {
         id
       },
-      select: {
-        id: true
-      }
     })
 
-    if (collaborator?.id) return collaborator.id
+    if (collaborator?.id) return collaborator
 
     return null
   }
@@ -60,6 +57,19 @@ export class CollaboratorRepoDB implements CollaboratorRepository{
       data: {
         registration_status: "APPROVED",
         updatedAt: new Date()
+      }
+    })
+
+    return
+  }
+
+  async updatePassword(id: string, password: string): Promise<void> {
+    await prisma.collaborator.update({
+      where: {
+        id
+      },
+      data: {
+        password
       }
     })
 
