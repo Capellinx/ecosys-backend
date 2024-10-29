b2294e8806613f032e3440587f11f0490030eca8
capellinx <capellaaa7@gmail.com>
2024-10-29 01:08:43 -0300

    feat: reduce length random pass

--- src/utils/generate-random-pass.ts
+++ src/utils/generate-random-pass.ts
@@ -1,6 +1,6 @@
 
 export class GenerateRandomPass {
    static generate(): string {
-      return Math.random().toString(20).substring(2, 15) + Math.random().toString(20).substring(2, 15);
+      return Math.random().toString(25).slice(-10)
    }
 }
