b67b2ce648aee1ad35c9eff9c459cd5df6669801
capellinx <capellaaa7@gmail.com>
2024-10-29 01:15:59 -0300

    feat: inject nodemailer and bcrypt in use case login

--- src/use-cases/collaborator/approve-collaborator/index.ts
+++ src/use-cases/collaborator/approve-collaborator/index.ts
@@ -1,10 +1,20 @@
 import { CollaboratorRepoDB } from "../../../infra/repositories/collaborator-repo-db";
+import { BcryptService } from "../../../infra/services/bcrypt.service";
+import { NodemailerService } from "../../../infra/services/nodemailer.service";
 import { ApproveCollaboratorUseCase } from "./approve-collaborator";
 import { ApproveCollaboratorController } from "./approve-collaborator.controller";
 
 const collaboratorRepoDb = new CollaboratorRepoDB()
 
-const approveCollaboratorUseCase = new ApproveCollaboratorUseCase(collaboratorRepoDb)
+const bcryptService = new BcryptService()
+
+const nodemailerService = new NodemailerService()
+
+const approveCollaboratorUseCase = new ApproveCollaboratorUseCase(
+   collaboratorRepoDb,
+   nodemailerService,
+   bcryptService
+)
 
 const approveCollaboratorController = new ApproveCollaboratorController(approveCollaboratorUseCase)
 
