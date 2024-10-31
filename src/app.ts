8da17be1d99dcef0e92fbddc5c88a46583b81ec9
capellinx <capellaaa7@gmail.com>
2024-10-30 22:40:08 -0300

    feat: configure cors

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,18 @@ import express, { json } from "express";
 import helmet from "helmet"
 import { errorHandler } from "./middleware/handle-errors.middleware";
 import { collaboratorRouter } from "./routes/collaborator.route";
+import cors from "cors"
+
 const app = express();
 
 app.use(json())
 app.use(helmet())
+app.use(cors({
+   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+   origin: "http://localhost:3000",
+   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}))
+
 
 app.use(collaboratorRouter)
 
